de1c02acef5bd6e22f28b6c1fe40ecaa4eebe236
SmaZone2020 <2431798772@qq.com>
2026-08-16 22:39:05 +0800

    (fix)ignore legacy AccessKey.Key field on deserialization

--- src/service/Data/MongoDbContext.cs
+++ src/service/Data/MongoDbContext.cs
@@ -1,5 +1,7 @@
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using Microsoft.Extensions.Options;
+using LibraNextgen.Common.Models;
 using LibraNextgen.Service.Configuration;
 
 namespace LibraNextgen.Service.Data;
@@ -8,6 +10,20 @@ public class MongoDbContext
 {
     public IMongoDatabase Database { get; }
     public IMongoClient Client { get; }
+
+    static MongoDbContext()
+    {
+        // AccessKey previously stored the raw `Key`; the field was renamed to
+        // KeyHash. Ignore extra elements so legacy documents still deserialize.
+        if (!BsonClassMap.IsClassMapRegistered(typeof(AccessKey)))
+        {
+            BsonClassMap.RegisterClassMap<AccessKey>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+            });
+        }
+    }
 
     public MongoDbContext(IOptions<MongoSettings> settings)
     {
