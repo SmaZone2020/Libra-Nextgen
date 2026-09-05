38b3eef45428be3cae9175fdc75c804338edbbec
SmaZone2020 <2431798772@qq.com>
2026-09-05 21:58:54 +0800

    (script) del script

--- scripts/e2e/tools/jwt-keygen/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Security.Cryptography;
-using System.Text;
-var bytes = File.ReadAllBytes(Path.Combine(
-    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-    "Libra-Nextgen", "jwt-rsa-key.bin"));
-var xml = Encoding.UTF8.GetString(ProtectedData.Unprotect(bytes, null, DataProtectionScope.CurrentUser));
-using var rsa = RSA.Create();
-rsa.FromXmlString(xml);
-Console.WriteLine(rsa.ExportPkcs8PrivateKeyPem());
